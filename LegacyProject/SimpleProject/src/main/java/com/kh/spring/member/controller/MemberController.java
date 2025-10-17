5fd21d9b9bfa2ea3802a9cb70dfe12001dc9d88e
unknown <cjysy0104@gmail.com>
2025-10-17 17:49:20 +0900

    1017

--- LegacyProject/SimpleProject/src/main/java/com/kh/spring/member/controller/MemberController.java
+++ LegacyProject/SimpleProject/src/main/java/com/kh/spring/member/controller/MemberController.java
@@ -1,6 +1,31 @@
 package com.kh.spring.member.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.spring.member.model.vo.Member;
+
+@Controller
 public class MemberController {
 
-	
+//	@RequestMapping("login")
+//	public void test1() {
+//		System.out.println("로그인 기능");
+//	}
+//	
+//	@RequestMapping("insert")
+//	public void test2() {
+//		System.out.println("가입 기능");
+//	}
+//	
+//	@RequestMapping("delete")
+//	public void test3() {
+//		System.out.println("탈퇴 기능");
+//	}
+	@RequestMapping("login")
+	public void login(Member member) {
+		// 1. 값뽑기
+		// 2. 데이터 가공 >> spring이 해줌
+		System.out.println(member);
+	}
 }
